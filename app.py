a29975d1a7ee4595a2664a5951b7a3e986735ae6
achpooja <pa2328138@gmail.com>
2024-02-22 00:43:01 +0545

    new file

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import warnings
+from keras.models import load_model
+from keras.applications.vgg16 import preprocess_input
+import numpy as np
+from keras.preprocessing import image
+import platform
+
+if platform.system() == "Windows":
+    from win32com.client import Dispatch
+
+warnings.filterwarnings('ignore')
+
+def speak(str1):
+    speak=Dispatch(("SAPI.SpVoice"))
+    speak.Speak(str1)
+
+def main():
+    st.title("Pneumonia Detection")
+    st.write("Upload a chest X-ray image to detect Pneumonia")
+
+    uploaded_file=st.file_uploader("Upload Image",type=["jpg","jpeg","png"])
+
+    if uploaded_file is not None:
+        st.subheader("Uploaded Image")
+        st.image(uploaded_file, caption='Uploaded image.',use_column_width=True)
+        st.write("Image uploaded successfully!")
+
+        #Button to Detect
+        if st.button("Detect Pneumonia"):
+            model=load_model('pneumoniaDetect.h5')
+            img=image.load_img(uploaded_file,target_size=(224,224))
+            x=image.img_to_array(img)
+            x=np.expand_dims(x,axis=0)
+            img_data=preprocess_input(x)
+            classes=model.predict(img_data)
+
+            if classes[0][0] > 0.5:
+                st.success("Result: No Pneumonia Detected")
+                speak("No Pneumonia Detected")
+            else:
+                st.error("Result: Pneumonia Detected")
+                speak("Pneumonia Detected")       
+
+if __name__ == '__main__':
+    main()                         
